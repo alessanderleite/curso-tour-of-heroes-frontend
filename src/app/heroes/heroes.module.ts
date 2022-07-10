31ffe98182f320265b0d3926439842e62d6e30b5
Alessander <alessander.logistica@gmail.com>
2022-07-10 16:00:10 -0300

    mock json server

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -4,7 +4,6 @@ import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
 import { MaterialModule } from '../material/material.module';
 import { FormsModule } from '@angular/forms';
-import { DashboardModule } from '../dashboard/dashboard.module';
 import { HeroesRoutingModule } from './heroes-routing.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
